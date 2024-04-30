be0134b60b5f2828bea9fbf91731935693969348
mohanex <mohamedpack@gmail.com>
2024-05-01 01:45:09 +0200

    ALL WORKING CASES

--- roman_to_integer/code.py
+++ roman_to_integer/code.py
@@ -1,5 +1,5 @@
 class Solution:
-    def romanToInt(s: str) -> int:
+    def romanToInt(self, s: str) -> int:
         res = 0
         array_of_s = list(s)
         save_x = 0
@@ -14,40 +14,41 @@ class Solution:
                     if save_i == 1 :
                         res = res+3
                     else : 
-                        res = res+6
+                        res = res+5
                     save_i = 0
                 case "X":
                     if save_i == 1 :
                         res = res+8
                     else : 
                         save_x = 1
+                        res = res+10
                     save_i = 0
                 case "L":
                     if save_x == 1 :
-                        res = res+40
+                        res = res+30
                     else : 
-                        res = res+60
+                        res = res+50
                     save_x = 0
                 case "C":
-                    save_c = 0
                     if save_x == 1 :
-                        res = res+90
+                        res = res+80
                     else : 
                         save_c = 1
+                        res = res+100
                     save_x = 0
                 case "D":
                     if save_c == 1 :
-                        res = res+400
+                        res = res+300
                     else : 
-                        res = res+600
+                        res = res+500
                     save_c = 0
                 case "M":
                     if save_c == 1 :
-                        res = res+900
+                        res = res+800
                     else : 
-                        res = res+1100
+                        res = res+1000
                     save_c = 0
         return res
     
 class main():
-    print(Solution.romanToInt("III"))
+    print(Solution.romanToInt("LVIII"))
