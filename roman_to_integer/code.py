1ffe581111d1748e70337ae531bcb56e104d537c
mohanex <mohamedpack@gmail.com>
2024-04-30 23:29:37 +0200

    adding code

--- roman_to_integer/code.py
+++ roman_to_integer/code.py
@@ -5,18 +5,23 @@ class Solution:
         for i in array_of_s:
             match i:
                 case "I":
-                    save = 1
+                    save_i = 1
                 case "V":
-                    if save == 1 :
+                    if save_i == 1 :
                         res = res+4
-                    else res = res+6
+                    else : 
+                        res = res+6
+                    save_i = 0
                 case "X":
-                    if save == 1 :
+                    if save_i == 1 :
                         res = res+9
-                    else res = res+11
+                    else : 
+                        save_x = 1
+                    save_i = 0
                 case "L":
                     res = res+50
                 case "C":
+                    save_c = 1
                     res = res+100
                 case "D":
                     res = res+500
